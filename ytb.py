02be74e613572998c5a37664404049c67d4b2c0a
louis-git-hub <107806927+louis-git-hub@users.noreply.github.com>
2023-12-10 16:39:57 +0100

    Update ytb.py

--- ytb.py
+++ ytb.py
@@ -1,18 +1,20 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, render_template
 from pytube import YouTube
 import os
 
 app = Flask(__name__)
 
-@app.route('/download', methods=['GET'])
-def download_video():
-    url = request.args.get('url')
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')  # Assurez-vous d'avoir un fichier index.html dans un dossier templates
+
+@app.route('/download', methods=['POST'])
+def download():
+    url = request.form['url']  # Récupération de l'URL à partir du formulaire
     yt = YouTube(url)
     video = yt.streams.get_highest_resolution()
-    video.download('/path/to/download')
-
-    # Après le téléchargement, envoyez le fichier au client
-    return send_file('/path/to/download/video.mp4', as_attachment=True)
+    download_path = video.download('/chemin/de/destination')
+    return f'Vidéo téléchargée : {download_path}'
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5001)  # Désactiver le mode debug et changer le port
+    app.run(debug=True)
